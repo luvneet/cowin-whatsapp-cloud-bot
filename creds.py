8deac4086170358c575e76cf9f86187c21c35c1a
Lovneet <83899995+luvneet@users.noreply.github.com>
2021-05-09 16:42:56 +0530

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,7 @@
+ACCOUNT_SID = ''  # put your Twilio Account sid here
+AUTH_TOKEN = ''  # put your Twilio Auth token here
+TWILIO_ALLOTED_NUMBER = ''  # put your Twilio alloted number here
+# Header if necessary otherwise Govt. API thinks request is coming from bot , not from actual browser and block your request.
+HEADER = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
+YOUR_PHONE_NO = ''  # put your phone no with country code here for error updates (eg : +919876543210)
